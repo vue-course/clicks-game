88b7edcb46aafbd7215172e93eada52c2cf69da9
David Meir-Levy <davidmeirlevy@gmail.com>
2019-02-25 21:06:03 +0200

    room selector

---
+++ src/components/RoomSelector.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<p>{{$game.step.message}}</p>
+		<ul>
+			<li v-for="room in $game.step.options"
+			    :key="room"
+			    :style="{backgroundColor: room}"
+			    @click="saveRoom(room)">{{room}}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'RoomSelector',
+		methods: {
+			saveRoom(room) {
+				this.$game.updateRoom(room);
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		display: flex;
+		flex-direction: row;
+		align-content: space-between;
+
+		li {
+			flex-grow: 2;
+			text-align: center;
+			padding: 50px 0;
+			cursor: pointer;
+		}
+	}
+</style>
